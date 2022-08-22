d1471a506dca0c517652404997cec6367f2af80b
GaelleBriet <gaelle.briet@protonmail.com>
2022-08-22 14:59:08 +0200

    refactoring by adding Timestampable trait

---
+++ src/Entity/Traits/Timestampable.php
@@ -0,0 +1,49 @@
+<?php
+
+namespace App\Entity\Traits;
+
+use Doctrine\DBAL\Types\Types;
+use Doctrine\ORM\Mapping as ORM;
+
+trait Timestampable
+{
+  #[ORM\Column(type: Types::DATETIME_MUTABLE, options: ["default" => "CURRENT_TIMESTAMP"])]
+  private ?\DateTimeInterface $createdAt = null;
+
+  #[ORM\Column(type: Types::DATETIME_MUTABLE, options: ["default" => "CURRENT_TIMESTAMP"])]
+  private ?\DateTimeInterface $updatedAt = null;
+
+  public function getCreatedAt(): ?\DateTimeInterface
+  {
+    return $this->createdAt;
+  }
+
+  public function setCreatedAt(\DateTimeInterface $createdAt): self
+  {
+    $this->createdAt = $createdAt;
+
+    return $this;
+  }
+
+  public function getUpdatedAt(): ?\DateTimeInterface
+  {
+    return $this->updatedAt;
+  }
+
+  public function setUpdatedAt(\DateTimeInterface $updatedAt): self
+  {
+    $this->updatedAt = $updatedAt;
+
+    return $this;
+  }
+
+  #[ORM\PrePersist]
+  #[ORM\PreUpdate]
+  public function updateTimestamps()
+  {
+    if ($this->getCreatedAt() === null) {
+      $this->setCreatedAt(new \DateTimeImmutable);
+    }
+    $this->setUpdatedAt(new \DateTimeImmutable);
+  }
+}
