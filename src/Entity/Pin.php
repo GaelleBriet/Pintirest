3db77e368a497a29914b321288bda2b91910aab3
GaelleBriet <gaelle.briet@protonmail.com>
2022-08-23 12:36:25 +0200

    Add validation

--- src/Entity/Pin.php
+++ src/Entity/Pin.php
@@ -4,9 +4,10 @@ namespace App\Entity;
 
 use App\Entity\Traits\Timestampable;
 use App\Repository\PinRepository;
-use DateTimeImmutable;
 use Doctrine\DBAL\Types\Types;
 use Doctrine\ORM\Mapping as ORM;
+use Symfony\Component\Validator\Constraints as Assert;
+
 
 #[ORM\Entity(repositoryClass: PinRepository::class)]
 #[ORM\Table(name: "pins")]
@@ -21,9 +22,19 @@ class Pin
     private ?int $id = null;
 
     #[ORM\Column(length: 255)]
+    #[Assert\NotBlank]
+    #[Assert\Length(
+        min: 3,
+        minMessage: 'Your title must be at least {{ limit }} characters long',
+    )]
     private ?string $title = null;
 
     #[ORM\Column(type: Types::TEXT)]
+    #[Assert\NotBlank]
+    #[Assert\Length(
+        min: 10,
+        minMessage: 'Your description must be at least {{ limit }} characters long',
+    )]
     private ?string $description = null;
 
 
