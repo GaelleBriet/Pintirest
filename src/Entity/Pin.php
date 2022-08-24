b122c741c573806cae297d9920dcd5fdd1d0435a
GaelleBriet <gaelle.briet@protonmail.com>
2022-08-24 17:18:33 +0200

    Add relations betxeen pins and users tables

--- src/Entity/Pin.php
+++ src/Entity/Pin.php
@@ -51,6 +51,10 @@ class Pin
     #[ORM\Column(length: 255, nullable: true)]
     private ?string $imageName = null;
 
+    #[ORM\ManyToOne(inversedBy: 'pins')]
+    #[ORM\JoinColumn(nullable: false)]
+    private ?User $user = null;
+
 
 
 
@@ -110,6 +114,18 @@ class Pin
     public function setImageName(?string $imageName): self
     {
         $this->imageName = $imageName;
+
+        return $this;
+    }
+
+    public function getUser(): ?User
+    {
+        return $this->user;
+    }
+
+    public function setUser(?User $user): self
+    {
+        $this->user = $user;
 
         return $this;
     }
