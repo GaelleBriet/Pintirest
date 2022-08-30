6f70430c5e784f5b8f8371bc2f8603d4d600cac0
GaelleBriet <gaelle.briet@protonmail.com>
2022-08-30 15:53:21 +0200

    Add change password

--- src/Controller/AccountController.php
+++ src/Controller/AccountController.php
@@ -2,12 +2,15 @@
 
 namespace App\Controller;
 
+use App\Entity\User;
+use App\Form\ChangePasswordFormType;
 use App\Form\UserFormType;
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Security\Core\Encoder\UserPasswordEncoderInterface;
 
 class AccountController extends AbstractController
 {
@@ -16,6 +19,7 @@ class AccountController extends AbstractController
     {
         return $this->render('account/show.html.twig');
     }
+
     #[Route('/account/edit', name: 'app_account_edit', methods: "GET|POST")]
     public function edit(Request $request, EntityManagerInterface $em): Response
     {
@@ -34,6 +38,31 @@ class AccountController extends AbstractController
 
 
         return $this->render('account/edit.html.twig', [
+            'form' => $form->createView()
+        ]);
+    }
+
+    #[Route('/account/change-password', name: 'app_account_change_password', methods: "GET|POST")]
+    public function changePassword(Request $request, EntityManagerInterface $em, UserPasswordEncoderInterface $passwordEncoder): Response
+    {
+        $user = $this->getUser();
+        $form = $this->createForm(ChangePasswordFormType::class);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $user->setPassword(
+                $passwordEncoder->encodePassword($user, $form['newPassword']->getData())
+            );
+
+            $em->flush();
+
+            $this->addFlash('success', 'Password updated successfully');
+
+            return $this->redirectToRoute('app_account');
+        }
+
+
+        return $this->render('account/change_password.html.twig', [
             'form' => $form->createView()
         ]);
     }
