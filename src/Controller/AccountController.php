a56db84f4ad666cd013fdf0539ffdf9a83c2305c
GaelleBriet <gaelle.briet@protonmail.com>
2022-08-30 16:48:11 +0200

    little path modification

--- src/Controller/AccountController.php
+++ src/Controller/AccountController.php
@@ -12,15 +12,16 @@ use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\Security\Core\Encoder\UserPasswordEncoderInterface;
 
+#[Route('/account')]
 class AccountController extends AbstractController
 {
-    #[Route('/account', name: 'app_account', methods: 'GET')]
+    #[Route('', name: 'app_account', methods: 'GET')]
     public function show(): Response
     {
         return $this->render('account/show.html.twig');
     }
 
-    #[Route('/account/edit', name: 'app_account_edit', methods: "GET|POST")]
+    #[Route('/edit', name: 'app_account_edit', methods: "GET|POST")]
     public function edit(Request $request, EntityManagerInterface $em): Response
     {
         $user = $this->getUser();
@@ -42,7 +43,7 @@ class AccountController extends AbstractController
         ]);
     }
 
-    #[Route('/account/change-password', name: 'app_account_change_password', methods: "GET|POST")]
+    #[Route('/change-password', name: 'app_account_change_password', methods: "GET|POST")]
     public function changePassword(Request $request, EntityManagerInterface $em, UserPasswordEncoderInterface $passwordEncoder): Response
     {
         $user = $this->getUser();
