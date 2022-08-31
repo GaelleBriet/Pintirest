e3a404e8196b40bd27414d289d091ead01f83f08
GaelleBriet <gaelle.briet@protonmail.com>
2022-08-31 14:50:02 +0200

    Add PinVoter and simplify access control checks

--- src/Controller/PinsController.php
+++ src/Controller/PinsController.php
@@ -7,6 +7,8 @@ use App\Form\PinType;
 use App\Repository\PinRepository;
 use App\Repository\UserRepository;
 use Doctrine\ORM\EntityManagerInterface;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\IsGranted;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Security;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
@@ -26,8 +28,11 @@ class PinsController extends AbstractController
     }
 
     #[Route('/pins/create', name: 'app_pins_create', methods: "GET|POST")]
+    #[Security("is_granted('ROLE_USER') and user.isVerified()")]
+    // #[Security("is_granted('ROLE_USER')")]
     public function create(Request $request, EntityManagerInterface $em, UserRepository $userRepo): Response
     {
+
         $pin = new Pin;
 
         $form = $this->createForm(PinType::class, $pin);
@@ -57,8 +62,14 @@ class PinsController extends AbstractController
     }
 
     #[Route('/pins/{id<[0-9]+>}/edit', name: 'app_pins_edit', methods: "GET|PUT")]
+    #[IsGranted('PIN_EDIT', subject: 'pin')]
     public function edit(Request $request, Pin $pin, EntityManagerInterface $em): Response
     {
+        // on ne vérifie plus ici si l'utilisateur est connecté mais on le fait dans les attributs (annotation) Security qui renvoie vers le PinVoter
+        // if ($pin->getUser() != $this->getUser()) {
+        //     throw $this->createAccessDeniedException();
+        // }
+
         $form = $this->createForm(PinType::class, $pin, [
             'method' => 'PUT'
         ]);
@@ -80,8 +91,14 @@ class PinsController extends AbstractController
     }
 
     #[Route('/pins/{id<[0-9]+>}', name: 'app_pins_delete', methods: "DELETE")]
+    // #[Security("is_granted('PIN_DELETE', pin)")]
+    #[IsGranted('PIN_DELETE', subject: 'pin')]
     public function delete(Request $request, Pin $pin, EntityManagerInterface $em): Response
     {
+        // if ($pin->getUser() != $this->getUser()) {
+        //     throw $this->createAccessDeniedException();
+        // }
+
         if ($this->isCsrfTokenValid('pin_deletion_' . $pin->getId(), $request->request->get('csrf_token'))) {
             $em->remove($pin);
             $em->flush();
--- src/Security/LoginFormAuthenticator.php
+++ src/Security/LoginFormAuthenticator.php
@@ -8,6 +8,7 @@ use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Generator\UrlGeneratorInterface;
 use Symfony\Component\Security\Core\Authentication\Token\TokenInterface;
+use Symfony\Component\Security\Core\Exception\AuthenticationException;
 use Symfony\Component\Security\Core\Security;
 use Symfony\Component\Security\Http\Authenticator\AbstractLoginFormAuthenticator;
 use Symfony\Component\Security\Http\Authenticator\Passport\Badge\CsrfTokenBadge;
@@ -58,5 +59,17 @@ class LoginFormAuthenticator extends AbstractLoginFormAuthenticator
     protected function getLoginUrl(Request $request): string
     {
         return $this->urlGenerator->generate(self::LOGIN_ROUTE);
+    }
+
+    /**
+     * Override to control what happens when the user hits a secure page
+     * but isn't logged in yet.
+     */
+    public function start(Request $request, AuthenticationException $authException = null): Response
+    {
+        $request->getSession()->getFlashBag()->add('error', 'You need to login first!');
+        $url = $this->getLoginUrl($request);
+
+        return new RedirectResponse($url);
     }
 }
